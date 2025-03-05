e00629f553f3488d64d6330783a9a1f99ee8a63d
Nigar Sultana Kana <nigarkana@gmail.com>
2025-03-05 19:53:51 +0000

    radioBtn and checkBtn automated (#1)
    * radioBtn and checkBtn automated
    
    * palywright inspector is closed
    
    * headless is true

--- playwright.config.js
+++ playwright.config.js
@@ -18,7 +18,7 @@ const config = ({
 
   use: {
     browserName : 'chromium',
-    headless : false
+    headless : true
    
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
