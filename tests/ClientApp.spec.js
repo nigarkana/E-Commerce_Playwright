714e72223fcacd28d245e04dccde293d5b1cfaef
Nigar Sultana Kana <nigarkana@gmail.com>
2025-03-14 01:11:39 -0700

    End to end testing has done

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -49,11 +49,34 @@ test ("End 2 End Testing of Client App", async({browser})=>{
           break;  
         }
     }
-
     //tested code
     await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
     await page.locator(".action__submit").click();
     await expect(await page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
     const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
     console.log("Order Id is: ", orderId);
+    //await page.pause();
+
+    //Orders page automation
+    //const orderID = "67d1c4b6c019fb1ad623a42a";
+    await page.locator("button[routerlink = '/dashboard/myorders']").click();
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+    //const orderHistory = await page.locator("th[scope ='row']");
+    //const orderCount = await orderHistory.count();
+    for (let i = 0; i < await rows.count(); ++i){
+        {
+            //Add to cart, playwright feature to locate "add to cart"
+            const rowOrderId = await rows.nth(i).locator("th").textContent();
+            if(orderId.includes(rowOrderId))
+            {
+                await rows.nth(i).locator("button").first().click();
+                break;
+            }
+            
+        }
+        //await page.waitForTimeout(5000);
+        const orderIdDetails = await page.locator(".col-text").textContent();
+        await expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    }
 })
